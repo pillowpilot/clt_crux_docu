f839017359898d8f066e3ca852a8a8e5e08a5a78
Carlos Gaona <carlos.gaona@clt.com.py>
2024-10-14 08:25:32 -0300

    Initial commit from Create Next App

---
+++ eventcatalog.config.js
@@ -0,0 +1,27 @@
+/** @type {import('@eventcatalog/core/bin/eventcatalog.config').Config} */
+export default {
+  title: 'EventCatalog',
+  tagline: 'Discover, Explore and Document your Event Driven Architectures',
+  organizationName: 'CLT',
+  homepageLink: 'https://eventcatalog.dev/',
+  editUrl: 'https://github.com/boyney123/eventcatalog-demo/edit/master',
+  // By default set to false, add true to get urls ending in /
+  trailingSlash: false,
+  // Change to make the base url of the site different, by default https://{website}.com/docs,
+  // changing to /company would be https://{website}.com/company/docs,
+  base: '/',
+  // Customize the logo, add your logo to public/ folder
+  logo: {
+    alt: 'EventCatalog Logo',
+    src: '/logo.png',
+    text: 'EventCatalog'
+  },
+  docs: {
+    sidebar: {
+      // Should the sub heading be rendered in the docs sidebar?
+      showPageHeadings: true
+    }
+  },
+  // required random generated id used by eventcatalog
+  cId: '999ae939-042a-41e3-8590-1d7ad4878ac1'
+}
